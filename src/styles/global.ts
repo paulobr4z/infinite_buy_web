19cf8cce7cbd3185d015080a81e2033d5e5137be
paulobr4z <paulobrazaraujo@gmail.com>
2023-10-23 22:22:56 -0300

    configuração inicial

---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    font-family: 'Poppins', sans-serif;
+    background-color: #f5f5f5;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  button, a {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`
