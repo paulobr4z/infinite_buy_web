d506bb181d5c7267facc6134ca6267619df6a1ff
Karine <karinedeveloper@gmail.com>
2023-10-31 13:23:58 -0300

    feat: adiciona theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { GlobalStyle } from './styles/global.ts'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './theme/index.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
