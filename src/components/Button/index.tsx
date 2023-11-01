12f720ef65e789507f6c568de53dc9b6f956bcf7
Karine <karinedeveloper@gmail.com>
2023-11-01 19:05:31 -0300

    feat: adiciona Icon no component Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,24 @@
 import { ButtonHTMLAttributes } from 'react'
 import { ButtonContainer } from './styled'
 import { CircularSpinner } from '../CircularSpinner'
+import { IconType } from 'react-icons'
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size: 'small' | 'medium' | 'large'
   loading?: boolean
+  Icon: IconType
 }
 
-export const Button = ({ children, size, loading, ...props }: ButtonProps) => {
+export const Button = ({
+  children,
+  size,
+  loading,
+  Icon,
+  ...props
+}: ButtonProps) => {
   return (
     <ButtonContainer size={size} {...props}>
-      {loading ? <CircularSpinner /> : children}
+      <Icon /> {loading ? <CircularSpinner /> : children}
     </ButtonContainer>
   )
 }
