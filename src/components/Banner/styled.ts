bf33c029cee46e2261ebced746ec2da6d2306d3f
Karine <karinedeveloper@gmail.com>
2023-12-04 22:10:44 -0300

    style: centraliza div do banner

--- src/components/Banner/styled.ts
+++ src/components/Banner/styled.ts
@@ -1,6 +1,12 @@
 import { styled } from 'styled-components'
 
 export const BannerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 5rem;
+
   img {
     width: 120rem;
     height: 30rem;
