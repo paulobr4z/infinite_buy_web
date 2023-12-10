3e6988348d53467f3efc469738eab420262e6bc3
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-10 19:39:34 -0300

    fix: Altera estilo SearchInput

--- src/components/Search/styled.ts
+++ src/components/Search/styled.ts
@@ -3,30 +3,27 @@ import { styled } from 'styled-components'
 export const SearchInput = styled.div`
   height: 3.5rem;
   display: flex;
+  align-items: center;
   width: 70%;
+  border-radius: 4px 0px 0px 4px;
+  border: 1px solid rgba(99, 99, 99, 0.21);
+  padding-right: 1.6rem;
+
   input {
     width: 100%;
-    border-radius: 4px 0px 0px 4px;
-    border: 1px solid rgba(99, 99, 99, 0.21);
-
+    border: none;
+    padding-left: 1.6rem;
+    font-size: ${({ theme }) => theme.fonts.text.medium};
     &:placeholder-shown {
       padding-left: 1.6rem;
       opacity: 0.3;
       font-size: ${({ theme }) => theme.fonts.text.medium};
     }
   }
-`
 
-export const ButtonSearch = styled.button`
-  border-radius: 0px 4px 4px 0px;
-  font-weight: ${({ theme }) => theme.fonts.weight.semiBold};
-  font-size: ${({ theme }) => theme.fonts.text.small};
-  color: ${({ theme }) => theme.colors.text.light};
-  padding: 8px 32px;
-  transition: 0.3s ease-in-out;
-  background: ${({ theme }) => theme.colors.primary};
-
-  &:hover {
-    opacity: 0.7;
+  svg {
+    color: rgba(99, 99, 99, 0.21);
+    font-size: 2rem;
+    cursor: pointer;
   }
 `
