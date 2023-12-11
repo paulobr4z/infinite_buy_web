5fdef3000a5e163f0167838f564768d91a3d44dd
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-10 21:02:38 -0300

    styles: Implementa responsivo em Search

--- src/components/Search/styled.ts
+++ src/components/Search/styled.ts
@@ -1,13 +1,19 @@
 import { styled } from 'styled-components'
+import { MobileBreakPoint } from '../../const/breackPoint'
 
 export const SearchInput = styled.div`
   height: 3.5rem;
   display: flex;
   align-items: center;
   width: 70%;
-  border-radius: 4px 0px 0px 4px;
+  border-radius: 0.4rem;
   border: 1px solid rgba(99, 99, 99, 0.21);
   padding-right: 1.6rem;
+  margin: 1rem 0;
+
+  @media (max-width: ${MobileBreakPoint}) {
+    width: 100%;
+  }
 
   input {
     width: 100%;
@@ -18,6 +24,10 @@ export const SearchInput = styled.div`
       padding-left: 1.6rem;
       opacity: 0.3;
       font-size: ${({ theme }) => theme.fonts.text.medium};
+
+      @media (max-width: ${MobileBreakPoint}) {
+        font-size: ${({ theme }) => theme.fonts.text.extraSmall};
+      }
     }
   }
 
