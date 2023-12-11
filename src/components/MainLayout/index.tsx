84c6a77cc849111cec9a70817bba651f25e4a2e1
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-10 21:00:58 -0300

    feat: Implementa ResponsiveHeader em MainLayout

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,16 +1,21 @@
-import { Header } from '../Header'
+// import { Header } from '../Header'
 import { Footer } from '../Footer'
 import { ReactNode } from 'react'
 import * as S from './styled'
+import { ResponsiveHeader } from '../Header/ResponsiveHeader'
+import { Header } from '../Header'
+import { useMediaQuery } from 'react-responsive'
 
 interface MainLayoutProps {
   children: ReactNode
 }
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
+  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 700px)' })
   return (
     <S.MainLayoutWrapper>
-      <Header />
+      {isDesktopOrLaptop ? <Header /> : <ResponsiveHeader />}
+
       {children}
       <Footer />
     </S.MainLayoutWrapper>
