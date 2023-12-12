f885fa3de32126e819514cbba99a943a5adc6259
paulobr4z <paulobrazaraujo@gmail.com>
2023-12-12 00:06:02 -0300

    fix: build error/import images banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react'
-import promo1 from '../../../public/promo1.png'
-import promo2 from '../../../public/promo2.jpg'
-import promo3 from '../../../public/promo3.png'
-import promo4 from '../../../public/promo4.jpg'
-
 import * as S from './styled'
 
-const promotionImages = [promo1, promo2, promo3, promo4]
+const promotionImages = [
+  '/promo1.png',
+  '/promo2.jpg',
+  '/promo3.png',
+  '/promo4.jpg',
+]
+
 export const Banner = () => {
   const [currentPromotionIndex, setCurrentPromotionIndex] = useState(0)
 
