f56f98fde5bbf50622ddb1ae5b72193feb267310
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-17 20:15:23 -0300

    Implementa lib react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { Banner } from './components/Banner'
-import { CardContainer } from './components/CardContainer'
-import { MainLayout } from './components/MainLayout'
 import { CartProvider } from './context/ContextCart'
+import { InfinitBuyRoutes } from './routes/routes'
 
 export const App = () => {
   return (
     <>
       <CartProvider>
-        <MainLayout>
-          <Banner />
-          <CardContainer />
-        </MainLayout>
+        <InfinitBuyRoutes />
       </CartProvider>
     </>
   )
