4082839340f458f7fd0ee58d050f3031e83031c2
Kelvin Charles <kelvinc1291@hotmail.com>
2023-12-19 21:09:50 -0300

    fix: Implementa corpo da page Signup

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,3 +1,24 @@
+import { Form } from './styled'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiLockPasswordLine, RiGroupLine } from 'react-icons/ri'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { Link } from 'react-router-dom'
+import { routes } from '../../routes'
+
 export const Signup = () => {
-  return <div>CADASTROOOO</div>
+  return (
+    <Form>
+      <h2>Cadastro</h2>
+      <p>
+        Já está cadastrado, faça <Link to={routes.login}>login</Link>
+      </p>
+
+      <Input Icon={RiGroupLine} placeholder="Nome completo" type="text" />
+      <Input Icon={MdOutlineEmail} placeholder="E-mail" type="email" />
+      <Input Icon={RiLockPasswordLine} placeholder="Senha" />
+      <Input Icon={RiLockPasswordLine} placeholder="Confirme sua senha" />
+      <Button size="large">Entrar</Button>
+    </Form>
+  )
 }
