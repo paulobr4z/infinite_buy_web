418ca64454793fcf357ca66db39da8c2367ccb6c
Kelvin Charles <kelvinc1291@hotmail.com>
2024-01-15 00:06:42 -0300

    feat: implementa message.ts para menssagens de erro

---
+++ src/utils/message.ts
@@ -0,0 +1,18 @@
+export const errorsMessage = {
+  name: {
+    required:
+      'O primeiro nome deve ter pelo menos 3 letras e inserir pelo menos um sobrenome.',
+  },
+  email: {
+    required: 'Email obrigatório',
+    invalid: 'Formato de email inválido',
+  },
+  password: {
+    required: 'Senha obrigatória',
+    min: 'Sua senha precisa ter no  mínimo 8 caracteres',
+  },
+  passwordConfirmation: {
+    required: 'Confirmação de senha obrigatória',
+    ref: 'As senhas devem corresponder',
+  },
+}
