71df72981ef04df5accb4bc8354cfeb84a967cb5
Kelvin Charles <kelvinc1291@hotmail.com>
2024-01-16 21:26:03 -0300

    fix: Adiciona IconRight em Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,6 +5,7 @@ import * as S from './styled'
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   Icon: IconType
   children?: ReactNode
+  iconRight?: ReactNode
 }
 
 // export const Input: React.FC<InputProps> = (
@@ -21,11 +22,12 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 // }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  function InputComponent({ Icon, children, ...props }, ref) {
+  function InputComponent({ Icon, children, iconRight, ...props }, ref) {
     return (
       <S.InputContainer>
         <Icon />
         <input ref={ref} {...props} />
+        {iconRight && iconRight}
         {children}
       </S.InputContainer>
     )
