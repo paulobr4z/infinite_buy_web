c7ab1c2723a27cae7ae89875f3c68cba5078b14f
Karine <karinedeveloper@gmail.com>
2024-01-21 17:47:29 -0300

    feat: adiciona responsividade ao input

--- src/components/Input/styled.ts
+++ src/components/Input/styled.ts
@@ -1,6 +1,11 @@
 import { styled } from 'styled-components'
+import { TabletBreakPoint } from '../../const/breackPoint'
 
-export const InputContainer = styled.div`
+export interface InputContainerProps {
+  size: 'medium' | 'large'
+}
+
+export const InputContainer = styled.div<InputContainerProps>`
   background-color: ${({ theme }) => theme.colors.background};
   border-radius: 1.6rem;
   padding: 1.8rem 2.6rem;
@@ -8,13 +13,14 @@ export const InputContainer = styled.div`
   font-weight: ${({ theme }) => theme.fonts.weight.light};
   display: flex;
   gap: 0.8rem;
-  width: 48rem;
+  max-width: 48rem;
 
   input {
     border: none;
     flex: 1;
     color: ${({ theme }) => theme.colors.text.secondaryAccent};
     background-color: transparent;
+    max-width: 48rem;
 
     &:focus {
       outline: none;
@@ -29,5 +35,10 @@ export const InputContainer = styled.div`
   svg {
     color: ${({ theme }) => theme.colors.text.secondaryAccent};
     opacity: 0.3;
+  }
+
+  @media (max-width: ${TabletBreakPoint}) {
+    width: ${({ size }) => (size === 'medium' ? '138px' : '249px')};
+    padding: 12px 26px;
   }
 `
--- src/const/breackPoint.ts
+++ src/const/breackPoint.ts
@@ -1,1 +1,2 @@
 export const MobileBreakPoint = '700px'
+export const TabletBreakPoint = '1024px'
