d1fd6e89e61c47ccaeba21dea9ec35663cb16041
Kelvin Charles <kelvinc1291@hotmail.com>
2024-01-22 23:27:08 -0300

    feat: Implementa modal em SubMenu

--- src/components/SubMenu/index.tsx
+++ src/components/SubMenu/index.tsx
@@ -1,13 +1,44 @@
 import { RxHamburgerMenu } from 'react-icons/rx'
 import * as S from './styled'
 import { ButtonCategorie } from '../ButtonCategorie'
+import Modal from 'react-modal'
+import { useState } from 'react'
+import { customStylesModalCategories } from '../../const/customStylesModal'
+import { ClosedIcon } from '../Cart/styled'
+import { SideBar } from '../SideBar'
+Modal.setAppElement('#root')
 
 export const SubMenu = () => {
+  const [modalIsOpen, setIsOpen] = useState(false)
+
+  const openModal = () => {
+    setIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+
   return (
     <S.Nav>
+      <div>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStylesModalCategories}
+          contentLabel="Modal Cart"
+        >
+          <S.TitleCategories>
+            <h3>Categorias</h3>
+            <ClosedIcon onClick={closeModal} />
+          </S.TitleCategories>
+          <SideBar />
+        </Modal>
+      </div>
+
       <S.Lista>
         <li>
-          <ButtonCategorie size="small">
+          <ButtonCategorie size="small" onClick={openModal}>
             <RxHamburgerMenu />
             Ver todas categorias
           </ButtonCategorie>
