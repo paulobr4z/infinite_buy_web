29f295534c048b2ad8bf3c6008c0fa3e7adeea79
Kelvin Charles <kelvinc1291@hotmail.com>
2024-01-22 23:29:52 -0300

    fix: Corrige arquitetura de Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,12 @@
 import { Banner } from '../../components/Banner'
 import { CardContainer } from '../../components/CardContainer'
 import { MainLayout } from '../../components/MainLayout'
-import { SideBar } from '../../components/SideBar'
 
 export const Home = () => {
   return (
-    <>
-      <SideBar />
-      <MainLayout>
-        <Banner />
-        <CardContainer />
-      </MainLayout>
-    </>
+    <MainLayout>
+      <Banner />
+      <CardContainer />
+    </MainLayout>
   )
 }
