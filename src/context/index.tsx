2c8425c7460a34dc646916367b428644989dabc4
Kelvin Charles <kelvinc1291@hotmail.com>
2024-01-23 22:16:15 -0300

    feat: Implementa contexto de produtos ao index de context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,5 +1,6 @@
 import { AuthProvider } from './AuthContext'
 import { CartProvider } from './CartContext'
+import { ProductsProvider } from './ProductsContext'
 
 interface AppProviderProps {
   children: React.ReactNode
@@ -8,7 +9,9 @@ interface AppProviderProps {
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <AuthProvider>
-      <CartProvider>{children}</CartProvider>
+      <ProductsProvider>
+        <CartProvider>{children}</CartProvider>
+      </ProductsProvider>
     </AuthProvider>
   )
 }
